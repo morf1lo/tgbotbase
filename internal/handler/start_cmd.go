db53ff30aeb11d3bf27039b91868982a074d64fd
morf1lo <abdullinti@gmail.com>
2024-12-17 13:33:49 +0500

    getting message from config with specified language

--- internal/handler/start_cmd.go
+++ internal/handler/start_cmd.go
@@ -1,7 +1,15 @@
 package handler
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/morf1lo/tgbotbase/internal/config"
+	"github.com/morf1lo/tgbotbase/internal/localization"
+)
 
 func (h *Handler) StartCMD(tgbot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	h.SendSimpleMessage(tgbot, update.Message.Chat.ID, "Hello!")
+	h.SendSimpleMessage(
+		tgbot,
+		update.Message.Chat.ID,
+		localization.GetMessage(config.ENGLISH_LANG, "startCMD.message"),
+	)
 }
